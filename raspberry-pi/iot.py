a82b234ba269d1751e17a8475c50ad55a76aeafe
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-07-31 15:10:28 +1000

    iot include pressure

--- raspberry-pi/iot.py
+++ raspberry-pi/iot.py
@@ -7,16 +7,17 @@ import time
 sense = SenseHat()
 sense.clear()
 
-temp = sense.get_temperature()
+temperature = sense.get_temperature()
 humidity = sense.get_humidity()
+pressure = sense.get_pressure()
 data = {
     "deviceid": deviceid.getserial(),
     "humidity": humidity,
-    "temperature": temp,
+    "pressure": pressure,
+    "temperature": temperature,
     "timestamp": int(round(time.time() * 1000)),
 }
 dataStr = json.dumps(data)
-print(data)
 myMQTTClient = AWSIoTMQTTClient("raspberry-RMIT")
 myMQTTClient.configureEndpoint("a2jwan9azy4oaj.iot.ap-southeast-2.amazonaws.com", 8883)
 myMQTTClient.configureCredentials("./root-ca.pem", "./f3ed0017cd-private.pem.key", "./f3ed0017cd-certificate.pem.crt")
