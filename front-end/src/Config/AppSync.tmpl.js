f04fe4bab0fc9d25567a36eb2144d9cd3b2b4fb2
Kendrick Kesley <3642811@student.rmit.edu.au>
2018-08-01 15:49:34 +1000

    remove app sync

--- front-end/src/Config/AppSync.tmpl.js
+++ front-end/src/Config/AppSync.tmpl.js
@@ -1,4 +1,9 @@
 export default {
+    apiKey: "API KEY",
+    region: 'REGION',
+    graphqlEndpoint: 'https://xxxxxxxxx.appsync-api.{REGION}.amazonaws.com/graphql'
+}
+export default {
     apiKey: 'da2-cy2hdfnhpfek5knjpgrakv5mem', //expires 7th august
     region: 'ap-southeast-2',
     graphqlEndpoint: 'https://pcqs3omaljbghgj5cf7jfmykyi.appsync-api.ap-southeast-2.amazonaws.com/graphql'
