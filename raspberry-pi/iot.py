5050f6c11d47a3ac3e2782f7f947808a100fcfce
kkesley <kendrick.kesley.dev@gmail.com>
2018-08-06 17:59:57 +1000

    iot per 5 seconds

--- raspberry-pi/iot.py
+++ raspberry-pi/iot.py
@@ -1,25 +1,31 @@
 import iotcore
 import reading
 import json
+import time
 
-# initialize system reading for reading environment
-sysreader = reading.Reading()
+def main():
+    # initialize system reading for reading environment
+    sysreader = reading.Reading()
 
-# get reading from environment
-data = sysreader.getReading()
-dataStr = json.dumps(data)
-print(dataStr)
+    # get reading from environment
+    data = sysreader.getReading()
+    dataStr = json.dumps(data)
+    print(dataStr)
 
-try:
-    # initialize iot connection
-    iotConnector = iotcore.IOTCore()
-    # publish data to iot
-    iotConnector.publish("rmit", dataStr)
-    # disconnect from iot
-    iotConnector.disconnect()
-except KeyError:
-    print("Invalid config file")
-except FileNotFoundError:
-    print("config file not found")
-except:
-    print("Cannot publish to iot core")
+    try:
+        # initialize iot connection
+        iotConnector = iotcore.IOTCore()
+        # publish data to iot
+        iotConnector.publish("rmit", dataStr)
+        # disconnect from iot
+        iotConnector.disconnect()
+    except KeyError:
+        print("Invalid config file")
+    except FileNotFoundError:
+        print("config file not found")
+    except:
+        print("Cannot publish to iot core")
+
+while True:
+    main()
+    time.sleep(5)
